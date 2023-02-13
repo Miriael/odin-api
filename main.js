821a9969406a673afa59d86ce3bbc93061c685e7
Miriael <ravafiel@protonmail.com>
2023-02-13 23:53:15 +0100

    Convert to async/await

--- main.js
+++ main.js
@@ -1,27 +1,15 @@
 import './style.css'
-
-let searchName = 'cats'
 const img = document.querySelector('img');
-const refetchButton = document.querySelector('#refetch')
-fetch('https://api.giphy.com/v1/gifs/translate?api_key=YKIhJpM6DUDn49DWlU31YGVET3wIpThu&s=' + searchName, {mode: 'cors'})
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(response) {
-    img.src = response.data.images.original.url;
-  });
+const refetchButton = document.querySelector('#refetch');
+
+async function getGifs(searchName) {
+  const response = await fetch('https://api.giphy.com/v1/gifs/translate?api_key=YKIhJpM6DUDn49DWlU31YGVET3wIpThu&s=' + searchName, {mode: 'cors'})
+  const gifData = await response.json();
+  img.src = gifData.data.images.original.url;
+}
 
 refetchButton.addEventListener('click', () => {
-  searchName = searchTerm.value
-  fetch('https://api.giphy.com/v1/gifs/translate?api_key=YKIhJpM6DUDn49DWlU31YGVET3wIpThu&s=' + searchName, {mode: 'cors'})
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(response) {
-    img.src = response.data.images.original.url;
-  })  
-  .catch(function(err) {
-    img.src = 'https://www.ncenet.com/wp-content/uploads/2020/04/No-image-found.jpg'
-  });
+  getGifs(document.querySelector('#searchTerm').value);
 })
 
+getGifs('cats');
